0e6046c0306972e3ab07f8a0bf42b065bc4c098c
Enrico Giurin <enricogiurin@gmail.com>
2026-06-24 01:24:56 +0200

    first draft rest-api working

---
+++ jobtracker-api/src/main/java/org/enricogiurin/jobtracker/api/JobtrackerApiApplication.java
@@ -0,0 +1,13 @@
+package org.enricogiurin.jobtracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtrackerApiApplication.class, args);
+	}
+
+}
